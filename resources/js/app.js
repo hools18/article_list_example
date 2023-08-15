6ad50da4792c1ac9be24912231f02399853f6120
HoolS18 <xhools18x@gmail.com>
2023-08-15 21:33:52 +0300

    add Vue 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,15 @@ import './bootstrap';
 import '../css/app.css';
 
 
+import {createApp} from "vue/dist/vue.esm-bundler.js";
+
+const app = createApp({});
+
+import Login from "./components/auth/LoginComponent.vue";
+import Register from "./components/auth/RegisterComponent.vue";
+
+
+app.component("login", Login)
+    .component("register", Register);
+
+app.mount("#app");
