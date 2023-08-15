03e3ccc970621f0c9611291271b50f5a0eb05e48
roman <romandolgenko@gmail.com>
2023-08-15 21:34:30 +0300

    create install seeder, fix html, fix roles

--- app/Domain/Article/Routes/route.php
+++ app/Domain/Article/Routes/route.php
@@ -5,9 +5,11 @@ use App\Domain\Article\Http\Controllers\ArticleController;
 Route::group(['prefix' => 'article', 'as' => 'article.'], function () {
     Route::get('', [ArticleController::class, 'index'])
         ->name('index');
-    Route::middleware(['auth'])
+
+    Route::middleware(['role:author'])
         ->get('/create', [ArticleController::class, 'create'])
         ->name('create');
+
     Route::get('/{article_slug}', [ArticleController::class, 'show'])
         ->name('show');
 });
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,26 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+@include('layout.blocks.head')
 <body class="antialiased">
-
 @include('layout.blocks.navbar')
 @include('layout.blocks.header')
+
 <main id="app">
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         @yield('content')
     </div>
 </main>
+
 @include('layout.blocks.footer')
 </body>
 </html>
