9900e6b988210cb44d110ca56968db4774b72cd0
roman <romandolgenko@gmail.com>
2023-08-19 18:04:05 +0300

    clickhouse fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
 
 class MainController
 {
     public function index(): View
     {
+        //        $r = LogVisit::select(['id', 'userId', 'visitDate', 'pageURL', 'pageType'])
+//            ->where('id', '=', 1)
+//            ->getRows();
+//
+//        dd($r);
+//        $db = DB::connection('clickhouse')->getClient();
+//        $statement = $db->insert(
+//            'log_visits',
+//            [
+//                [1, 1, time(), 'articles/show/1', 'articles'],
+//                [2, 1, time(), 'articles/show/2', 'articles'],
+//                [3, 1, time(), 'articles/show/3', 'articles'],
+//                [4, 1, time(), 'articles/show/4', 'articles'],
+//            ],
+//            ['id', 'userId', 'visitDate', 'pageURL', 'pageType']
+//        );
+//        dd($statement);
+
         return view('main.index');
     }
 }
